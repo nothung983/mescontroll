7328f517e63291d2c9d2ba0dcc923091e2fee21f
LyKhai2708 <khaib2111930@student.ctu.edu.vn>
2025-03-09 12:51:35 +0700

    Revert "fix my device "

--- MesControlApp/MesControlApp/mydevice.Designer.cs
+++ MesControlApp/MesControlApp/mydevice.Designer.cs
@@ -30,10 +30,6 @@
         {
             label1 = new Label();
             listView1 = new ListView();
-            column1 = new ColumnHeader();
-            column2 = new ColumnHeader();
-            column3 = new ColumnHeader();
-            column4 = new ColumnHeader();
             btnAdd = new Button();
             SuspendLayout();
             // 
@@ -44,57 +40,41 @@
             label1.ForeColor = Color.FromArgb(44, 62, 80);
             label1.Location = new Point(20, 10);
             label1.Name = "label1";
-            label1.Size = new Size(144, 32);
+            label1.Size = new Size(153, 41);
             label1.TabIndex = 0;
             label1.Text = "My Devices";
             // 
             // listView1
             // 
-            listView1.Columns.AddRange(new ColumnHeader[] { column1, column2, column3, column4 });
-            listView1.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            listView1.FullRowSelect = true;
-            listView1.GridLines = true;
             listView1.Location = new Point(20, 40);
             listView1.Name = "listView1";
             listView1.Size = new Size(540, 250);
             listView1.TabIndex = 1;
             listView1.UseCompatibleStateImageBehavior = false;
             listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+            listView1.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            listView1.Columns.AddRange(new[] {
+                new ColumnHeader { Text = "Device Type", Width = 100 },
+                new ColumnHeader { Text = "Name", Width = 200 },
+                new ColumnHeader { Text = "Brand", Width = 100 },
+                new ColumnHeader { Text = "Booking Status", Width = 120 }
+            });
             listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
-            // 
-            // column1
-            // 
-            column1.Text = "Device Type";
-            column1.Width = 100;
-            // 
-            // column2
-            // 
-            column2.Text = "Name";
-            column2.Width = 200;
-            // 
-            // column3
-            // 
-            column3.Text = "Brand";
-            column3.Width = 100;
-            // 
-            // column4
-            // 
-            column4.Text = "Booking Status";
-            column4.Width = 120;
             // 
             // btnAdd
             // 
-            btnAdd.BackColor = Color.FromArgb(46, 204, 113);
-            btnAdd.FlatAppearance.BorderSize = 0;
-            btnAdd.FlatStyle = FlatStyle.Flat;
-            btnAdd.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnAdd.ForeColor = Color.White;
             btnAdd.Location = new Point(400, 300);
             btnAdd.Name = "btnAdd";
             btnAdd.Size = new Size(100, 35);
             btnAdd.TabIndex = 2;
             btnAdd.Text = "Add Device";
-            btnAdd.UseVisualStyleBackColor = false;
+            btnAdd.BackColor = Color.FromArgb(46, 204, 113);
+            btnAdd.ForeColor = Color.White;
+            btnAdd.FlatStyle = FlatStyle.Flat;
+            btnAdd.FlatAppearance.BorderSize = 0;
+            btnAdd.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnAdd.Click += btnAdd_Click;
             // 
             // mydevice
@@ -110,7 +90,6 @@
             Name = "mydevice";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "My Devices";
-            Load += mydevice_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -120,10 +99,6 @@
         private Label label1;
         private ListView listView1;
         private Button btnAdd;
-        private ColumnHeader column1;
-        private ColumnHeader column2;
-        private ColumnHeader column3;
-        private ColumnHeader column4;
     }
 }
 //namespace MesControlApp
