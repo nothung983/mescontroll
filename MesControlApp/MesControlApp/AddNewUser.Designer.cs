f2412b5fd364418ebb450e0ddf7e446230f08e70
Dang Hung <hungb2111984@student.ctu.edu.vn>
2025-03-12 21:04:35 +0700

    Add new user UI

---
+++ MesControlApp/MesControlApp/AddNewUser.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace MesControlApp
+{
+    partial class AddNewUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            usr_image_Btn = new Button();
+            usr_image = new PictureBox();
+            user_phone_txt = new TextBox();
+            usr_email_txt = new TextBox();
+            user_name_txt = new TextBox();
+            SavesBtn = new Button();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            addnewlb = new Label();
+            user_pass_txt = new TextBox();
+            label5 = new Label();
+            retype_pass_txt = new TextBox();
+            label6 = new Label();
+            usr_role_cmb = new ComboBox();
+            label7 = new Label();
+            menuStrip1 = new MenuStrip();
+            homeToolStripMenuItem = new ToolStripMenuItem();
+            myDeviceToolStripMenuItem = new ToolStripMenuItem();
+            bookingToolStripMenuItem = new ToolStripMenuItem();
+            devicesToolStripMenuItem = new ToolStripMenuItem();
+            bookingToolStripMenuItem1 = new ToolStripMenuItem();
+            myAccountToolStripMenuItem = new ToolStripMenuItem();
+            myAccountToolStripMenuItem1 = new ToolStripMenuItem();
+            logOutToolStripMenuItem = new ToolStripMenuItem();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)usr_image).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(usr_image_Btn);
+            groupBox1.Controls.Add(usr_image);
+            groupBox1.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox1.Location = new Point(46, 88);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(278, 361);
+            groupBox1.TabIndex = 30;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "User Image";
+            // 
+            // usr_image_Btn
+            // 
+            usr_image_Btn.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            usr_image_Btn.Location = new Point(49, 306);
+            usr_image_Btn.Name = "usr_image_Btn";
+            usr_image_Btn.Size = new Size(185, 32);
+            usr_image_Btn.TabIndex = 13;
+            usr_image_Btn.Text = "Upload New Image";
+            usr_image_Btn.UseVisualStyleBackColor = true;
+            // 
+            // usr_image
+            // 
+            usr_image.Location = new Point(32, 39);
+            usr_image.Name = "usr_image";
+            usr_image.Size = new Size(213, 261);
+            usr_image.SizeMode = PictureBoxSizeMode.CenterImage;
+            usr_image.TabIndex = 12;
+            usr_image.TabStop = false;
+            // 
+            // user_phone_txt
+            // 
+            user_phone_txt.Location = new Point(396, 198);
+            user_phone_txt.Name = "user_phone_txt";
+            user_phone_txt.Size = new Size(328, 27);
+            user_phone_txt.TabIndex = 29;
+            // 
+            // usr_email_txt
+            // 
+            usr_email_txt.Location = new Point(396, 137);
+            usr_email_txt.Name = "usr_email_txt";
+            usr_email_txt.Size = new Size(328, 27);
+            usr_email_txt.TabIndex = 28;
+            // 
+            // user_name_txt
+            // 
+            user_name_txt.Location = new Point(396, 77);
+            user_name_txt.Name = "user_name_txt";
+            user_name_txt.Size = new Size(328, 27);
+            user_name_txt.TabIndex = 27;
+            // 
+            // SavesBtn
+            // 
+            SavesBtn.BackColor = Color.MediumSeaGreen;
+            SavesBtn.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            SavesBtn.ForeColor = Color.White;
+            SavesBtn.Location = new Point(488, 439);
+            SavesBtn.Name = "SavesBtn";
+            SavesBtn.Size = new Size(167, 40);
+            SavesBtn.TabIndex = 26;
+            SavesBtn.Text = "Save";
+            SavesBtn.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            label4.Location = new Point(396, 170);
+            label4.Name = "label4";
+            label4.Size = new Size(185, 25);
+            label4.TabIndex = 25;
+            label4.Text = "User Phone Number";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            label3.Location = new Point(396, 109);
+            label3.Name = "label3";
+            label3.Size = new Size(102, 25);
+            label3.TabIndex = 24;
+            label3.Text = "User Email";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            label2.Location = new Point(396, 49);
+            label2.Name = "label2";
+            label2.Size = new Size(98, 25);
+            label2.TabIndex = 23;
+            label2.Text = "Username";
+            // 
+            // addnewlb
+            // 
+            addnewlb.AutoSize = true;
+            addnewlb.Font = new Font("Poppins", 14F, FontStyle.Bold);
+            addnewlb.Location = new Point(46, 43);
+            addnewlb.Name = "addnewlb";
+            addnewlb.Size = new Size(187, 42);
+            addnewlb.TabIndex = 22;
+            addnewlb.Text = "Add New User";
+            addnewlb.Click += label1_Click;
+            // 
+            // user_pass_txt
+            // 
+            user_pass_txt.Location = new Point(396, 262);
+            user_pass_txt.Name = "user_pass_txt";
+            user_pass_txt.Size = new Size(328, 27);
+            user_pass_txt.TabIndex = 32;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            label5.Location = new Point(396, 234);
+            label5.Name = "label5";
+            label5.Size = new Size(134, 25);
+            label5.TabIndex = 31;
+            label5.Text = "User Password";
+            // 
+            // retype_pass_txt
+            // 
+            retype_pass_txt.Location = new Point(396, 326);
+            retype_pass_txt.Name = "retype_pass_txt";
+            retype_pass_txt.Size = new Size(328, 27);
+            retype_pass_txt.TabIndex = 34;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            label6.Location = new Point(396, 298);
+            label6.Name = "label6";
+            label6.Size = new Size(163, 25);
+            label6.TabIndex = 33;
+            label6.Text = "Re-type Password";
+            // 
+            // usr_role_cmb
+            // 
+            usr_role_cmb.FormattingEnabled = true;
+            usr_role_cmb.Location = new Point(396, 394);
+            usr_role_cmb.Name = "usr_role_cmb";
+            usr_role_cmb.Size = new Size(328, 28);
+            usr_role_cmb.TabIndex = 35;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            label7.Location = new Point(396, 366);
+            label7.Name = "label7";
+            label7.Size = new Size(92, 25);
+            label7.TabIndex = 36;
+            label7.Text = "User Role";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Font = new Font("Lexend Deca", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { homeToolStripMenuItem, devicesToolStripMenuItem, bookingToolStripMenuItem1, myAccountToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 32);
+            menuStrip1.TabIndex = 37;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            homeToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { myDeviceToolStripMenuItem, bookingToolStripMenuItem });
+            homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            homeToolStripMenuItem.Size = new Size(67, 28);
+            homeToolStripMenuItem.Text = "Home";
+            // 
+            // myDeviceToolStripMenuItem
+            // 
+            myDeviceToolStripMenuItem.Name = "myDeviceToolStripMenuItem";
+            myDeviceToolStripMenuItem.Size = new Size(180, 28);
+            myDeviceToolStripMenuItem.Text = "My Devices";
+            // 
+            // bookingToolStripMenuItem
+            // 
+            bookingToolStripMenuItem.Name = "bookingToolStripMenuItem";
+            bookingToolStripMenuItem.Size = new Size(180, 28);
+            bookingToolStripMenuItem.Text = "My Booking";
+            // 
+            // devicesToolStripMenuItem
+            // 
+            devicesToolStripMenuItem.Name = "devicesToolStripMenuItem";
+            devicesToolStripMenuItem.Size = new Size(81, 28);
+            devicesToolStripMenuItem.Text = "Devices";
+            // 
+            // bookingToolStripMenuItem1
+            // 
+            bookingToolStripMenuItem1.Name = "bookingToolStripMenuItem1";
+            bookingToolStripMenuItem1.Size = new Size(84, 28);
+            bookingToolStripMenuItem1.Text = "Booking";
+            // 
+            // myAccountToolStripMenuItem
+            // 
+            myAccountToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { myAccountToolStripMenuItem1, logOutToolStripMenuItem });
+            myAccountToolStripMenuItem.Name = "myAccountToolStripMenuItem";
+            myAccountToolStripMenuItem.Size = new Size(83, 28);
+            myAccountToolStripMenuItem.Text = "Account";
+            // 
+            // myAccountToolStripMenuItem1
+            // 
+            myAccountToolStripMenuItem1.Name = "myAccountToolStripMenuItem1";
+            myAccountToolStripMenuItem1.Size = new Size(179, 28);
+            myAccountToolStripMenuItem1.Text = "My Account";
+            // 
+            // logOutToolStripMenuItem
+            // 
+            logOutToolStripMenuItem.Name = "logOutToolStripMenuItem";
+            logOutToolStripMenuItem.Size = new Size(179, 28);
+            logOutToolStripMenuItem.Text = "Log out";
+            // 
+            // AddNewUser
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 491);
+            Controls.Add(menuStrip1);
+            Controls.Add(label7);
+            Controls.Add(usr_role_cmb);
+            Controls.Add(retype_pass_txt);
+            Controls.Add(label6);
+            Controls.Add(user_pass_txt);
+            Controls.Add(label5);
+            Controls.Add(groupBox1);
+            Controls.Add(user_phone_txt);
+            Controls.Add(usr_email_txt);
+            Controls.Add(user_name_txt);
+            Controls.Add(SavesBtn);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(addnewlb);
+            Name = "AddNewUser";
+            Text = "AddNewUser";
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)usr_image).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button usr_image_Btn;
+        private PictureBox usr_image;
+        private TextBox user_phone_txt;
+        private TextBox usr_email_txt;
+        private TextBox user_name_txt;
+        private Button SavesBtn;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label addnewlb;
+        private TextBox user_pass_txt;
+        private Label label5;
+        private TextBox retype_pass_txt;
+        private Label label6;
+        private ComboBox usr_role_cmb;
+        private Label label7;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem homeToolStripMenuItem;
+        private ToolStripMenuItem myDeviceToolStripMenuItem;
+        private ToolStripMenuItem bookingToolStripMenuItem;
+        private ToolStripMenuItem devicesToolStripMenuItem;
+        private ToolStripMenuItem bookingToolStripMenuItem1;
+        private ToolStripMenuItem myAccountToolStripMenuItem;
+        private ToolStripMenuItem myAccountToolStripMenuItem1;
+        private ToolStripMenuItem logOutToolStripMenuItem;
+    }
+}
