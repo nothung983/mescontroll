af21707750608210971e6f2d3d0a9a372158fddb
Dang Hung <hungb2111984@student.ctu.edu.vn>
2025-03-22 16:51:19 +0700

    fix database connection error

--- MesControlApp/MesControlApp/DatabaseConnection.cs
+++ MesControlApp/MesControlApp/DatabaseConnection.cs
@@ -1,4 +1,67 @@
-﻿using System;
+﻿//using System;
+//using System.Data;
+//using Media_Device_Management;
+//using Microsoft.Data.SqlClient;
+
+//namespace MesControlApp
+//{
+//    internal class DatabaseConnection
+//    {
+//        public static SqlConnection connection;
+
+//        public static bool Connect()
+//        {
+//            try
+//            {
+//                if (connection == null)
+//                {
+//                    string connectionString = "Data Source=DANGHUNG_LAPTOP\\MSSQLSERVER2022;Initial Catalog=mediaz_db;Integrated Security=True;MultipleActiveResultSets=True;TrustServerCertificate=True";
+//                    connection = new SqlConnection(connectionString);
+//                }
+
+//                if (connection.State == ConnectionState.Closed)
+//                {
+//                    connection.Open();
+//                }
+
+//                return true;
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine("Connection error: " + ex.Message);
+//                return false;
+//            }
+//        }
+
+//        public static bool CloseConnect()
+//        {
+//            try
+//            {
+//                if (connection != null && connection.State == System.Data.ConnectionState.Open)
+//                {
+//                    connection.Close();
+//                }
+//                return true;
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine("Error closing connection: " + ex.Message);
+//                return false;
+//            }
+//        }
+
+//        public static SqlConnection GetConnection()
+//        {
+//            if (connection == null)
+//            {
+//                Connect();  // Ensure connection is initialized
+//            }
+//            return connection;
+//        }
+//    }
+//}
+
+using System;
 using System.Data;
 using Microsoft.Data.SqlClient;
 
@@ -6,16 +69,24 @@ namespace MesControlApp
 {
     internal class DatabaseConnection
     {
-        public static SqlConnection connection;
+        private static SqlConnection connection;
 
+        // Chuỗi kết nối mặc định
+        private static readonly string connectionString =
+            "Data Source=DANGHUNG_LAPTOP\\MSSQLSERVER2022;Initial Catalog=mediaz_db;Integrated Security=True;MultipleActiveResultSets=True;TrustServerCertificate=True";
+
+        // Mở kết nối nếu cần
         public static bool Connect()
         {
             try
             {
                 if (connection == null)
                 {
-                    string connectionString = "Data Source=DANGHUNG_LAPTOP\\MSSQLSERVER2022;Initial Catalog=mediaz_db;Integrated Security=True;MultipleActiveResultSets=True;TrustServerCertificate=True";
                     connection = new SqlConnection(connectionString);
+                }
+                else if (string.IsNullOrWhiteSpace(connection.ConnectionString))
+                {
+                    connection.ConnectionString = connectionString;
                 }
 
                 if (connection.State == ConnectionState.Closed)
@@ -32,11 +103,12 @@ namespace MesControlApp
             }
         }
 
+        // Đóng kết nối
         public static bool CloseConnect()
         {
             try
             {
-                if (connection != null && connection.State == System.Data.ConnectionState.Open)
+                if (connection != null && connection.State != ConnectionState.Closed)
                 {
                     connection.Close();
                 }
@@ -49,8 +121,14 @@ namespace MesControlApp
             }
         }
 
+        // Trả về SqlConnection đang dùng
         public static SqlConnection GetConnection()
         {
+            if (connection == null || string.IsNullOrWhiteSpace(connection.ConnectionString))
+            {
+                Connect();  // Khởi tạo nếu cần
+            }
+
             return connection;
         }
     }
---
+++ MesControlApp/MesControlApp/Main_dashboard.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Media_Device_Management
+{
+    partial class Main_dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label2 = new Label();
+            panel3 = new Panel();
+            label3 = new Label();
+            panel5 = new Panel();
+            dataGridView1 = new DataGridView();
+            Recent_Booking = new Label();
+            menuStrip1 = new MenuStrip();
+            home_menu = new ToolStripMenuItem();
+            myDevice_menu = new ToolStripMenuItem();
+            mybooking_menu = new ToolStripMenuItem();
+            devices_menu = new ToolStripMenuItem();
+            allDevices_MenuItem = new ToolStripMenuItem();
+            booking_menu = new ToolStripMenuItem();
+            pendingBooking_menu = new ToolStripMenuItem();
+            allBooking_menu = new ToolStripMenuItem();
+            usersToolStripMenuItem = new ToolStripMenuItem();
+            userLists_menu = new ToolStripMenuItem();
+            Account_menu = new ToolStripMenuItem();
+            myAccount_menu = new ToolStripMenuItem();
+            logOut_menu = new ToolStripMenuItem();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(39, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 29);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(label1);
+            panel1.Font = new Font("Lexend Deca", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            panel1.Location = new Point(40, 88);
+            panel1.Margin = new Padding(3, 4, 3, 4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(147, 57);
+            panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(label2);
+            panel2.Font = new Font("Lexend Deca", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            panel2.Location = new Point(40, 177);
+            panel2.Margin = new Padding(3, 4, 3, 4);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(147, 57);
+            panel2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(39, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 29);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // panel3
+            // 
+            panel3.BorderStyle = BorderStyle.FixedSingle;
+            panel3.Controls.Add(label3);
+            panel3.Font = new Font("Lexend Deca", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            panel3.Location = new Point(40, 261);
+            panel3.Margin = new Padding(3, 4, 3, 4);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(147, 57);
+            panel3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(40, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(66, 29);
+            label3.TabIndex = 1;
+            label3.Text = "label3";
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(dataGridView1);
+            panel5.Location = new Point(249, 103);
+            panel5.Margin = new Padding(3, 4, 3, 4);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(510, 399);
+            panel5.TabIndex = 6;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(15, 17);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 24;
+            dataGridView1.Size = new Size(478, 362);
+            dataGridView1.TabIndex = 0;
+            // 
+            // Recent_Booking
+            // 
+            Recent_Booking.AutoSize = true;
+            Recent_Booking.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Recent_Booking.Location = new Point(249, 56);
+            Recent_Booking.Name = "Recent_Booking";
+            Recent_Booking.Size = new Size(183, 31);
+            Recent_Booking.TabIndex = 2;
+            Recent_Booking.Text = "Recent Booking";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Font = new Font("Lexend Deca", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { home_menu, devices_menu, booking_menu, usersToolStripMenuItem, Account_menu });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(830, 32);
+            menuStrip1.TabIndex = 24;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // home_menu
+            // 
+            home_menu.DropDownItems.AddRange(new ToolStripItem[] { myDevice_menu, mybooking_menu });
+            home_menu.Name = "home_menu";
+            home_menu.Size = new Size(67, 28);
+            home_menu.Text = "Home";
+            home_menu.Click += home_menu_Click_1;
+            // 
+            // myDevice_menu
+            // 
+            myDevice_menu.Name = "myDevice_menu";
+            myDevice_menu.Size = new Size(180, 28);
+            myDevice_menu.Text = "My Devices";
+            myDevice_menu.Click += myDevice_menu_Click;
+            // 
+            // mybooking_menu
+            // 
+            mybooking_menu.Name = "mybooking_menu";
+            mybooking_menu.Size = new Size(180, 28);
+            mybooking_menu.Text = "My Booking";
+            mybooking_menu.Click += mybooking_menu_Click;
+            // 
+            // devices_menu
+            // 
+            devices_menu.DropDownItems.AddRange(new ToolStripItem[] { allDevices_MenuItem });
+            devices_menu.Name = "devices_menu";
+            devices_menu.Size = new Size(81, 28);
+            devices_menu.Text = "Devices";
+            // 
+            // allDevices_MenuItem
+            // 
+            allDevices_MenuItem.Name = "allDevices_MenuItem";
+            allDevices_MenuItem.Size = new Size(173, 28);
+            allDevices_MenuItem.Text = "All Devices";
+            allDevices_MenuItem.Click += allDevices_MenuItem_Click;
+            // 
+            // booking_menu
+            // 
+            booking_menu.DropDownItems.AddRange(new ToolStripItem[] { pendingBooking_menu, allBooking_menu });
+            booking_menu.Name = "booking_menu";
+            booking_menu.Size = new Size(84, 28);
+            booking_menu.Text = "Booking";
+            // 
+            // pendingBooking_menu
+            // 
+            pendingBooking_menu.Name = "pendingBooking_menu";
+            pendingBooking_menu.Size = new Size(219, 28);
+            pendingBooking_menu.Text = "Pending Booking";
+            pendingBooking_menu.Click += pendingBooking_menu_Click;
+            // 
+            // allBooking_menu
+            // 
+            allBooking_menu.Name = "allBooking_menu";
+            allBooking_menu.Size = new Size(219, 28);
+            allBooking_menu.Text = "All Booking";
+            allBooking_menu.Click += allBooking_menu_Click;
+            // 
+            // usersToolStripMenuItem
+            // 
+            usersToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { userLists_menu });
+            usersToolStripMenuItem.Name = "usersToolStripMenuItem";
+            usersToolStripMenuItem.Size = new Size(64, 28);
+            usersToolStripMenuItem.Text = "Users";
+            // 
+            // userLists_menu
+            // 
+            userLists_menu.Name = "userLists_menu";
+            userLists_menu.Size = new Size(156, 28);
+            userLists_menu.Text = "All Users";
+            userLists_menu.Click += userLists_menu_Click;
+            // 
+            // Account_menu
+            // 
+            Account_menu.DropDownItems.AddRange(new ToolStripItem[] { myAccount_menu, logOut_menu });
+            Account_menu.Name = "Account_menu";
+            Account_menu.Size = new Size(83, 28);
+            Account_menu.Text = "Account";
+            // 
+            // myAccount_menu
+            // 
+            myAccount_menu.Name = "myAccount_menu";
+            myAccount_menu.Size = new Size(224, 28);
+            myAccount_menu.Text = "My Account";
+            myAccount_menu.Click += myAccount_menu_Click;
+            // 
+            // logOut_menu
+            // 
+            logOut_menu.Name = "logOut_menu";
+            logOut_menu.Size = new Size(224, 28);
+            logOut_menu.Text = "Log out";
+            logOut_menu.Click += logOut_menu_Click;
+            // 
+            // Main_dashboard
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(830, 562);
+            Controls.Add(menuStrip1);
+            Controls.Add(Recent_Booking);
+            Controls.Add(panel5);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Main_dashboard";
+            Text = "Dashboard";
+            Load += Main_dashboard_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Label Recent_Booking;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem home_menu;
+        private ToolStripMenuItem myDevice_menu;
+        private ToolStripMenuItem mybooking_menu;
+        private ToolStripMenuItem devices_menu;
+        private ToolStripMenuItem allDevices_MenuItem;
+        private ToolStripMenuItem booking_menu;
+        private ToolStripMenuItem pendingBooking_menu;
+        private ToolStripMenuItem allBooking_menu;
+        private ToolStripMenuItem usersToolStripMenuItem;
+        private ToolStripMenuItem userLists_menu;
+        private ToolStripMenuItem Account_menu;
+        private ToolStripMenuItem myAccount_menu;
+        private ToolStripMenuItem logOut_menu;
+    }
+}
